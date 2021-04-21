d2d946b12a1b78ef0450c3d6a29f261c4185f920
NadyaOvchinnikova <ovchinikova-n@mail.ru>
2021-04-21 20:03:07 +0500

    Задание 5.2

---
+++ Example_5_2_compare.py
@@ -0,0 +1,12 @@
+def compare(S1, S2):
+    ngrams = [
+        S1[i:i + 3] for i in range(len(S1))
+    ]
+    count = 0
+    for ngram in ngrams:
+        count += S2.count(ngram)
+    return count / max(len(S1), len(S2))
+
+
+a = compare('стол', 'стул')
+print(str(a))
